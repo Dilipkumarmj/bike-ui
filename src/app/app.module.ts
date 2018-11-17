305c5ce1cd96c6e2cfba95dd5b04c028ed7beba2
Dilipkumar MJ <mail2Dilipu@gmail.com>
2018-11-18 00:02:45 +0530

    added security code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,22 @@ import { BikeService} from './services/bike.service';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
-
+import { CallbackComponent } from './components/callback/callback.component';
+import {AuthService} from './services/auth.service';
+import {AuthGuard} from './services/auth.guard';
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     HomeComponent,
-    ViewRegistrationComponent
+    ViewRegistrationComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,HttpClientModule,ReactiveFormsModule
   ],
-  providers: [BikeService],
+  providers: [BikeService,AuthService,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
